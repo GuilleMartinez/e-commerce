3e21e50218517c8503ab203ed280b1dcd1999d19
Guillermo Martinez <gmartinezunpsjb@gmail.com>
2021-01-11 20:28:28 -0300

    Edición en código, se agrega la funcion de filtrado a través de búsqueda

--- src/js/app.js
+++ src/js/app.js
@@ -9,8 +9,10 @@ function createProductsList(JSON) {
 
 function renderProducts(productsList, shoppingCart) {
   const PRODUCTS_UL = document.getElementById("products");
+  PRODUCTS_UL.textContent = "";
+
   for (product of productsList) {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     const htmlProduct = product.renderHTML(productsList, shoppingCart);
     li.appendChild(htmlProduct);
     PRODUCTS_UL.appendChild(li);
@@ -31,35 +33,55 @@ function setKeywords(productList) {
   }
 
   function createOptions(keys) {
-    const datalist = document.getElementById('keywords');
+    const datalist = document.getElementById("keywords");
 
     for (key of keys) {
-      const option = document.createElement('option');
+      const option = document.createElement("option");
       option.value = key;
       datalist.appendChild(option);
     }
   }
-
-
 }
 
-function showCart () {
-  const modal = document.getElementById('shopping-cart');
+function showCart() {
+  const modal = document.getElementById("shopping-cart");
   if (modal.open) {
-      modal.close();
-  }
-  else {
+    modal.close();
+  } else {
     modal.show();
   }
-  
 }
+
+function setFilterByKeywords() {
+
+  const SEARCH_INPUT = document.getElementById("search-input");
+  SEARCH_INPUT.oninput = filterbyKey;
+
+  function filterbyKey(event) {
+
+    const word = event.target.value.toLowerCase();
+    if (word) {
+      const arr = PRODUCTS.filter((item) => item.keywords.includes(word));
+      if (arr.length) renderProducts(arr, CART);
+    } else {
+      renderProducts(PRODUCTS, CART);
+    }
+  }
+
+}
+
+function setCartRender() {
+  const SHOP_BTN = document.getElementById("cart-icon");
+  SHOP_BTN.onclick = showCart;
+}
+
 
 const PRODUCTS = createProductsList(DATABASE);
 const CART = new ShoppingCart();
-const SHOP_BTN = document.getElementById('cart-icon');
+
 
 renderProducts(PRODUCTS, CART);
 setKeywords(PRODUCTS);
-SHOP_BTN.onclick = showCart;
-
+setFilterByKeywords();
+setCartRender();
 
--- src/js/database.js
+++ src/js/database.js
@@ -74,4 +74,80 @@ const DATABASE = [
       keywords: "frutilla,frutillas,frutos rojos,fruta,frutas",
     },
   },
+
+  {
+    id: 5,
+    name: "Pomelo",
+    description: "Pomelo",
+    category: "Frutas",
+    brand: "Pixabay",
+    price: 200,
+    extra: {
+      hasDiscount: false,
+      discountValue: 0,
+      imgURL: "./src/img/frutas/pomelo.jpg",
+      keywords: "pomelo,pomelos,citricos,fruta,frutas",
+    },
+  },
+
+  {
+    id: 6,
+    name: "Manzana Verde",
+    description: "Manzana Verde",
+    category: "Frutas",
+    brand: "RP Photography",
+    price: 200,
+    extra: {
+      hasDiscount: false,
+      discountValue: 0,
+      imgURL: "./src/img/frutas/manzana_verde.jpg",
+      keywords: "manzana,manzanas,manzanas verdes,fruta,frutas",
+    },
+  },
+
+  {
+    id: 7,
+    name: "Manzana Roja",
+    description: "Manzana Roja",
+    category: "Frutas",
+    brand: "RP Photography",
+    price: 200,
+    extra: {
+      hasDiscount: false,
+      discountValue: 0,
+      imgURL: "./src/img/frutas/manzana_roja.jpg",
+      keywords: "manzana,manzanas,manzanas roja,fruta,frutas",
+    },
+  },
+
+  {
+    id: 8,
+    name: "Peras",
+    description: "Peras",
+    category: "Frutas",
+    brand: "Marta Dzedyshko",
+    price: 200,
+    extra: {
+      hasDiscount: false,
+      discountValue: 0,
+      imgURL: "./src/img/frutas/pera.jpg",
+      keywords: "pera,peras,fruta,frutas",
+    },
+  },
+
+  {
+    id: 9,
+    name: "Bananas",
+    description: "Bananas",
+    category: "Frutas",
+    brand: "Marta Dzedyshko",
+    price: 200,
+    extra: {
+      hasDiscount: false,
+      discountValue: 0,
+      imgURL: "./src/img/frutas/banana.jpg",
+      keywords: "banana,bananas,fruta,frutas",
+    },
+  },
+
 ];
